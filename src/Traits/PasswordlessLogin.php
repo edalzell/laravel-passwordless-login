225d4929a79fbb5edd5335ff92567c4dd05754ad
Viktor Szépe <viktor@szepe.net>
2026-02-26 02:55:56 +0100

    Improve PasswordlessLogin trait (#90)
    Co-authored-by: edalzell <erin@silentz.co>

--- src/Traits/PasswordlessLogin.php
+++ src/Traits/PasswordlessLogin.php
@@ -3,77 +3,53 @@
 namespace Grosv\LaravelPasswordlessLogin\Traits;
 
 use Grosv\LaravelPasswordlessLogin\LoginUrl;
+use Grosv\LaravelPasswordlessLogin\PasswordlessLogin as PasswordlessLoginFacade;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 
-/**
- * Logs in a user without a password.
- */
 trait PasswordlessLogin
 {
-    /**
-     * Returns the guard set for this user.
-     */
     public function getGuardNameAttribute(): string
     {
         return config('laravel-passwordless-login.user_guard');
     }
 
-    /**
-     * Whether a user should be remembered on login.
-     */
     public function getShouldRememberLoginAttribute(): bool
     {
         return config('laravel-passwordless-login.remember_login');
     }
 
-    /**
-     * Returns the number of minutes the route will expire in from the current time.
-     */
     public function getLoginRouteExpiresInAttribute(): int
     {
         return config('laravel-passwordless-login.login_route_expires');
     }
 
-    /**
-     * Returns the url to redirect to on successful login.
-     */
     public function getRedirectUrlAttribute(): string
     {
         return config('laravel-passwordless-login.redirect_on_success');
     }
 
-    /**
-     * Returns whether or not to use link once.
-     *
-     * @return bool
-     */
-    public function getLoginUseOnceAttribute()
+    public function getLoginUseOnceAttribute(): bool
     {
-        return config('laravel-passwordless-login.login_use_once');
+        return config()->boolean('laravel-passwordless-login.login_use_once');
     }
 
-    public function createPasswordlessLoginLink()
+    public function createPasswordlessLoginLink(): string
     {
         return (new LoginUrl($this))->generate();
     }
 
     /**
      * This is a callback called on a successful login.
-     *
-     *
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function onPasswordlessLoginSuccess($request)
+    public function onPasswordlessLoginSuccess(Request $request): RedirectResponse|Redirector
     {
         return ($request->has('redirect_to')) ? redirect($request->redirect_to) : redirect($this->getRedirectUrlAttribute());
     }
 
-    /**
-     * Generates the login link for this user.
-     *
-     * @return string
-     */
-    public function generateLoginUrl()
+    public function generateLoginUrl(): string
     {
-        return \Grosv\LaravelPasswordlessLogin\PasswordlessLogin::forUser($this)->generate();
+        return PasswordlessLoginFacade::forUser($this)->generate();
     }
 }
